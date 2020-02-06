5d1cbd0b74b64554d13943ae64224937c55bac00
Katie <2021SjobK75@moundsviewschools.org>
2020-02-05 19:53:01 -0600

    started on countdown

---
+++ assets/scripts/countdown.js
@@ -0,0 +1,13 @@
+
+var countDownDate = new Date(Feb 15, 2020 7:00:00).getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  $('#countdown').
+})
+
+https://www.w3schools.com/howto/howto_js_countdown.asp
