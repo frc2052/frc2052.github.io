4ff0c755eed7509d0d92a59940ebc86d442ca940
Katie <2021SjobK75@moundsviewschools.org>
2020-02-08 14:08:18 -0600

    made gif on home page only activate on mouse hover

---
+++ assets/scripts/hovergif.js
@@ -0,0 +1,7 @@
+
+$('#hovergif').mouseover(function() {
+  $(this).attr("src", "/assets/images/LogoAnimation.gif");
+});
+$('#hovergif').mouseout(function() {
+  $(this).attr("src", "/assets/images/logo-large.png");
+});
