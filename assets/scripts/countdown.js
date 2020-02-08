a9ee39a7bd5a11b9dde682cfd325be5b8a3ecad6
Katie <2021SjobK75@moundsviewschools.org>
2020-02-08 11:35:04 -0600

    Finished week zero countdown

--- assets/scripts/countdown.js
+++ assets/scripts/countdown.js
@@ -1,5 +1,5 @@
 
-var countDownDate = new Date(Feb 15, 2020 7:00:00).getTime();
+var countDownDate = new Date("Feb 15, 2020 7:00:00").getTime();
 var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = countDownDate - now;
@@ -7,7 +7,9 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  $('#countdown').
-})
-
-https://www.w3schools.com/howto/howto_js_countdown.asp
+  $('#countdown').text(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+  if (distance < 0) {
+    clearInterval(x);
+    $('#countdown').text("Week Zero has arrived!");
+  }
+}, 1000);
