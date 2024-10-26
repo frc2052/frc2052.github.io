46c7553af846724fa6db2f67376a46765c5b5a96
MasonLilley <102882288+MasonLilley@users.noreply.github.com>
2024-10-26 10:06:08 -0500

    restore font + lots of little stuff. history is broken

--- assets/scripts/searchteams.js
+++ assets/scripts/searchteams.js
@@ -1,18 +1,17 @@
 function searchTeams() {
     var input = $('input').val().toUpperCase();
-    var $shownElements, isNum = parseInt(input);
-
-    $('tr').hide().filter('tr.mnteamsHeader').show();
+    var isNum = parseInt(input);
 
     try {
-        if (isNum) {
-            $shownElements = $(`tr a[name*="${input}"]`);
-        } else {
-            $shownElements = $(`td[class="mnteamsList glossary-term"]`).filter(function() {
-                return $(this).text().trim().toUpperCase().includes(input);
-            });
-        }
-        
+        $('tr').hide().filter('tr.mnteamsHeader').show();
+
+        var $shownElements = isNum ? 
+        $(`tr a[name*="${input}"]`) 
+        : 
+        $(`td[class="mnteamsList glossary-term"]`).filter(function() {
+            return $(this).text().trim().toUpperCase().includes(input);
+        });
+
         $shownElements.each(function() {
             $(this).closest('tr').show();
         })
